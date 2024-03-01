e9305f8d2ab96c2e6a240e024e169413dcdfafa8
ajaykumar-21 <ajaykumarpk789@gmail.com>
2024-03-01 20:40:56 +0530

    add new code

--- modal/src/App.js
+++ modal/src/App.js
@@ -2,93 +2,87 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [dob, setDob] = useState("");
-  const [phone, setPhone] = useState("");
+  const [showForm, setShowForm] = useState(false);
+  const [obj, setObj] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    dob: "",
+  });
+  // Submit Form Functionality
+  function handleSubmit(e) {
+    e.preventDefault();
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-  const handleOpenModal = () => {
-    setIsOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsOpen(false);
-  };
-
-  const handleSubmit = () => {
-    if (!username || !email || !dob || !phone) {
-      alert("Please fill out all fields.");
-      return;
-    }
-
-    if (!email.includes("@")) {
+    if (!obj.email.match(emailRegex)) {
       alert("Invalid email. Please check your email address.");
-      return;
     }
 
-    if (phone.length !== 10 || isNaN(phone)) {
+    if (obj.phone.length !== 10) {
       alert("Invalid phone number. Please enter a 10-digit phone number.");
-      return;
     }
 
-    const today = new Date();
-    const enteredDate = new Date(dob);
-
-    if (enteredDate > today) {
-      alert("Invalid date of birth. Please enter a valid date.");
-      return;
+    if (new Date(obj.dob) > new Date()) {
+      alert("Invalid date of birth. Date of birth cannot be in the future.");
     }
 
-    // Here you can perform further actions like submitting the form data
-    // Reset the form
-    setUsername("");
-    setEmail("");
-    setDob("");
-    setPhone("");
-    setIsOpen(false);
-  };
+    setObj({ username: "", email: "", phone: "", dob: "" });
+  }
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setObj({ ...obj, [name]: value });
+  }
 
   return (
-    <div className="modal">
+    <div className="model-wrapper">
       <h1>User Details Modal</h1>
-      <button onClick={handleOpenModal}>Open Form</button>
-      {isOpen && (
-        <div className="modal-content" onClick={handleCloseModal}>
-          <div className="modal-form" onClick={(e) => e.stopPropagation()}>
+      <button onClick={() => setShowForm(true)}>Open Form</button>
+      {showForm && (
+        <>
+          <div className="modal" onClick={() => setShowForm(false)}></div>
+          <form className="modal-content" onSubmit={handleSubmit}>
+            <h1>Fill details</h1>
+            <label htmlFor="username">Username:</label>
             <input
               type="text"
-              placeholder="Username"
+              name="username"
               id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              value={obj.username}
+              onChange={handleChange}
+              required
             />
+            <label htmlFor="email">Email Address:</label>
             <input
               type="email"
-              placeholder="Email"
+              name="email"
               id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={obj.email}
+              onChange={handleChange}
+              required
             />
+            <label htmlFor="phone">Phone Number:</label>
             <input
-              type="text"
-              placeholder="Date of Birth"
-              id="dob"
-              value={dob}
-              onChange={(e) => setDob(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Phone"
+              type="tel"
+              name="phone"
               id="phone"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
+              value={obj.phone}
+              onChange={handleChange}
             />
-            <button className="submit-button" onClick={handleSubmit}>
+            <label htmlFor="dob">Date of Birth:</label>
+            <input
+              type="date"
+              name="dob"
+              id="dob"
+              value={obj.dob}
+              onChange={handleChange}
+              required
+            />
+            <button type="submit" className="submit-button">
               Submit
             </button>
-          </div>
-        </div>
+          </form>
+        </>
       )}
     </div>
   );
