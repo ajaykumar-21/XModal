a41e8346811ea2a54c20d01f122e2b907314fedf
ajaykumar-21 <ajaykumarpk789@gmail.com>
2024-03-01 21:16:21 +0530

    add new fn

--- modal/src/App.js
+++ modal/src/App.js
@@ -2,15 +2,28 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [showForm, setShowForm] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  // const [username, setUsername] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [dob, setDob] = useState("");
+  // const [phone, setPhone] = useState("");
+
   const [obj, setObj] = useState({
     username: "",
     email: "",
     phone: "",
     dob: "",
   });
-  // Submit Form Functionality
-  function handleSubmit(e) {
+
+  const handleOpenModal = () => {
+    setIsOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
@@ -27,7 +40,7 @@ function App() {
     }
 
     setObj({ username: "", email: "", phone: "", dob: "" });
-  }
+  };
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -35,54 +48,54 @@ function App() {
   }
 
   return (
-    <div className="model-wrapper">
+    <div className="modal">
       <h1>User Details Modal</h1>
-      <button onClick={() => setShowForm(true)}>Open Form</button>
-      {showForm && (
-        <>
-          <div className="modal" onClick={() => setShowForm(false)}></div>
-          <form className="modal-content" onSubmit={handleSubmit}>
+      <button onClick={handleOpenModal}>Open Form</button>
+      {isOpen && (
+        <div className="modal-content" onClick={handleCloseModal}>
+          <div className="modal-form" onClick={(e) => e.stopPropagation()}>
             <h1>Fill details</h1>
             <label htmlFor="username">Username:</label>
             <input
               type="text"
               name="username"
+              // placeholder="Username"
               id="username"
               value={obj.username}
               onChange={handleChange}
-              required
             />
             <label htmlFor="email">Email Address:</label>
             <input
               type="email"
               name="email"
+              // placeholder="Email"
               id="email"
               value={obj.email}
               onChange={handleChange}
-              required
             />
-            <label htmlFor="phone">Phone Number:</label>
+           <label htmlFor="dob">Date of Birth:</label>
             <input
-              type="tel"
+              type="date"
+              name="dob"
+              // placeholder="Date of Birth"
+              id="dob"
+              value={obj.dob}
+              onChange={handleChange}
+            />
+             <label htmlFor="phone">Phone Number:</label>
+            <input
+              type="text"
               name="phone"
+              // placeholder="Phone"
               id="phone"
               value={obj.phone}
               onChange={handleChange}
             />
-            <label htmlFor="dob">Date of Birth:</label>
-            <input
-              type="date"
-              name="dob"
-              id="dob"
-              value={obj.dob}
-              onChange={handleChange}
-              required
-            />
-            <button type="submit" className="submit-button">
+            <button className="submit-button" onClick={handleSubmit}>
               Submit
             </button>
-          </form>
-        </>
+          </div>
+        </div>
       )}
     </div>
   );
